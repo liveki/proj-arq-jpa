1012fe724247836bae1bbbe0d5fabadf3bff0f54
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2021-04-20 00:18:46 +0000

    refact(Evento.java): removing unusued imports in Evento.java

--- src/main/java/com/grupo1/grupo1/negocio/entidades/Evento.java
+++ src/main/java/com/grupo1/grupo1/negocio/entidades/Evento.java
@@ -1,13 +1,10 @@
 package com.grupo1.grupo1.negocio.entidades;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Column;
 
 @Entity
 public class Evento {
